1e6925801fbf923f4bfeb10e4f530ada98a3acaa
Kira <rarakira@gmail.com>
2021-05-13 15:56:28 +0300

    Update gitignore
    Initial commit
    
    Edit subtree
    
    Update udate
    
    Update to initial

--- ft_printf.c
+++ ft_printf.c
@@ -6,13 +6,14 @@
 /*   By: lbaela <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/13 13:51:22 by lbaela            #+#    #+#             */
-/*   Updated: 2021/05/13 14:36:32 by lbaela           ###   ########.fr       */
+/*   Updated: 2021/05/13 16:20:09 by lbaela           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft.h"
 
+/* Function that mimics the real printf */
 int ft_printf(const char *str, ...)
 {
 	(void) str;
