eec12a039121d607edfcb112da23815b444bd1c7
Kira <rarakira@gmail.com>
2021-05-13 15:14:43 +0300

    Initial commit

---
+++ ft_printf/includes/ft_printf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lbaela <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/05/13 13:43:41 by lbaela            #+#    #+#             */
+/*   Updated: 2021/05/13 14:33:02 by lbaela           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdarg.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stddef.h>
+# include <stdio.h>
+
+int	ft_printf(const char *str, ...);
+
+#endif
---
+++ ft_printf_mytest/main.c
@@ -0,0 +1,9 @@
+#include "ft_printf.h"
+#include "libft.h"
+#include <stdio.h>
+
+int	main(void)
+{
+	printf("Hello! All set & good!\n");
+	return (0);
+}
