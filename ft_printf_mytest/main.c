d744f1fc66e8710f60268ddf3eb3a620d638b14c
Kira <rarakira@gmail.com>
2021-05-17 14:35:50 +0300

    Count arg specifiers in string

--- ft_printf_mytest/main.c
+++ ft_printf_mytest/main.c
@@ -4,6 +4,10 @@
 
 int	main(void)
 {
-	printf("Hello! All set & good!\n");
+	ft_printf("Hello! All set & good!\n");
+	ft_printf("\n%%d, %%d, %%d\n");
+	ft_printf("%d, %d, %d\n", 5, 10, 20, 50, 100);
+	ft_printf("\n%%, %%d , %%d\n");
+	ft_printf("%%, %d, %d\n", 5, 10);
 	return (0);
 }
